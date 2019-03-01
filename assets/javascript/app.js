5062f7ace0c40281a0f48e4c3a7cbf4303cb3416
Robert H <robertjameshardin@aol.com>
2019-03-01 01:04:50 -0600

    Added more meta data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ let topics = ["ian mckellen", "elijah wood", "viggo mortensen", "orlando bloom",
 function actorButtons() {
 	//Removes existing buttons so they are not repeated each time function is called
 	$("#buttons").empty();
+	$("#buttons").html("<h1> Welcome to the Actor's Hangout! If you think actors are cool, you're in the right place! </h1>");
 	//For loop that runs the length of topics array
 	for (let i = 0; i < topics.length; i++) {
 		//Creates new buttons and assigns them to aBtn variable
@@ -11,11 +12,11 @@ function actorButtons() {
 		//Sets the attribute "data-name" to current string of topics array
 		aBtn.attr("data-name", topics[i]);
 		//Sets the text of each button to the currect string of topics array
-		aBtn.text(topics[i]);
+		aBtn.text("I think " + topics[i] + " is cool!");
 		//Appends each button to the buttons div
 		$("#buttons").append(aBtn);
 	}
-}
+};
 actorButtons();
 //On-click event that fires the ajax request
 $(".actor").on("click", function () {
@@ -39,8 +40,10 @@ $(".actor").on("click", function () {
 			let gifDiv = $("<div>");
 			//Retrieves the rating value from the JSON and assigns it to rating variable
 			let rating = results[i].rating;
+			let title = results[i].title;
+			let p1 = $("<p>").text("Title: " + title);
 			//Creates paragraph tag with text that displays "Rating:" and the rating value retrieved above
-			let p = $("<p>").text("Rating: " + rating);
+			let p2 = $("<p>").text("Rating: " + rating);
 			//Assigns a newly made image tag with a src contained inside retrieved JSON
 			//gif returns immediately in still state for user manipulation in a later function
 			let actorImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
@@ -50,24 +53,25 @@ $(".actor").on("click", function () {
 			actorImage.attr("data-state", "still");
 			actorImage.attr("class", "gif");
 			//Appends each gif rating and image to the gifDiv
-			gifDiv.append(p);
+			gifDiv.append(p1);
+			gifDiv.append(p2);
 			gifDiv.append(actorImage);
 			//Prepends the gifDiv onto the page, each response above the last
 			$("#gifs").prepend(gifDiv);
 		}
 	})
-})
+});
 //Function that adds a new actor after user types name and hits submit
 $("#add-actor").on("click", function(event) {
 	//Prevents form from refreshing the page and submitting data to nowhere
 	event.preventDefault();
 	//Captures the new actor value that user typed into box
-	let newActor = $("#actor-input").val().trim();
+	let newActor = $("#actor-input").val();
 	//Pushes the new actor into the topics array
 	topics.push(newActor);
 	//Calls actorButtons function to update new button and so exisiting buttons are not added again
 	actorButtons();
-})
+});
 //on-click event that lets user play and pause gif at will
 $(".gif").on("click", function() {
 	//Assigns state variable to the value of data-state attribute
@@ -85,4 +89,4 @@ $(".gif").on("click", function() {
 		//Returns data-state attribute to still
 		$(this).attr("data-state", "still");
 	}
-})
+});
