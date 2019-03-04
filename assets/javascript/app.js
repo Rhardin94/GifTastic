a5169391fa07af3d8d93e2eb5408b9d622cb6608
Robert H <robertjameshardin@aol.com>
2019-03-04 17:57:17 -0600

    Favorites grabs the last appended item

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ $(document.body).on("click", ".actor", function () {
 		url: queryURL,
 		method: "GET",
 		//Promise that runs this next function once it retrieves a response
-	}).then(function(response) {
+	}).then(function (response) {
 		//Logs the response to ensure request is working
 		console.log(response)
 		//Assigns each response JSON to the results variable
@@ -41,9 +41,9 @@ $(document.body).on("click", ".actor", function () {
 			//Retrieves the rating value from the JSON and assigns it to rating variable
 			let rating = results[i].rating;
 			let title = results[i].title;
-			let p1 = $("<p>").text("Title: " + title);
+			let pTitle = $("<p>").text("Title: " + title);
 			//Creates paragraph tag with text that displays "Rating:" and the rating value retrieved above
-			let p2 = $("<p>").text("Rating: " + rating);
+			let pRating = $("<p>").text("Rating: " + rating);
 			//Assigns a newly made image tag with a src contained inside retrieved JSON
 			//gif returns immediately in still state for user manipulation in a later function
 			let actorImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
@@ -52,48 +52,50 @@ $(document.body).on("click", ".actor", function () {
 			actorImage.attr("data-animate", results[i].images.fixed_height.url);
 			actorImage.attr("data-state", "still");
 			actorImage.attr("class", "gif");
-      //Appends each gif rating and image to the gifDiv
-      //Adding the favorite button to each img
-      let favButton = $("<button>").text("Add as favorite?");
+			//Appends each gif rating and image to the gifDiv
+			//Adding the favorite button to each img
+			let favButton = $("<button>").text("Add as favorite?");
       favButton.attr("id", "favButton");
-			gifDiv.append(p1);
-			gifDiv.append(p2);
-      gifDiv.append(actorImage);
-      gifDiv.prepend(favButton);
+			gifDiv.append(actorImage);      
+			gifDiv.append(pTitle);
+			gifDiv.append(pRating);
+			gifDiv.append(favButton);
 			//Prepends the gifDiv onto the page, each response above the last
 			$("#gifs").prepend(gifDiv);
 			//On-click event that adds gifs to favorites div
-			$(document.body).on("click", "#favButton", function() {
-  			$("#favorites").prepend($(this).gifDiv);
-			}),/*Displays if request fails*/ function(errorMessage) {
-				$(gifDiv).text("The request failed:" + errorMessage.code);
-			}
+			$(document.body).on("click", "#favButton", function () {
+          $("#favorites").append(gifDiv[i]);
+          $(this).hide();
+				}), /*Displays if request fails*/
+				function (errorMessage) {
+					$(gifDiv).text("The request failed:" + errorMessage.code);
+				}
 		}
 	})
 });
 //On-click event that adds gifs to favorites div
-$(document.body).on("click", "#favButton", function() {
-  $("#favorites").append($(this));
+$(document.body).on("click", "#favButton", function () {
+	$("#favorites").append($(this));
 });
 //Function that adds a new actor after user types name and hits submit
-$("#add-actor").on("click", function(event) {
+$("#add-actor").on("click", function (event) {
 	//Prevents form from refreshing the page and submitting data to nowhere
 	event.preventDefault();
 	//Captures the new actor value that user typed into box
 	let newActor = $("#actor-input").val();
 	//Prevent button from added empty buttons
-	if (($("#actor-input").val() === ""))  {
+	if (($("#actor-input").val() === "")) {
 		return false;
 	} else {
-	//Pushes the new actor into the topics array
-	topics.push(newActor);
-	//Calls actorButtons function to update new button and so exisiting buttons are not added again
-  actorButtons();
-	$("#actor-input").val("");
+		//Pushes the new actor into the topics array
+		topics.push(newActor);
+		//Calls actorButtons function to update new button and so exisiting buttons are not added again
+		actorButtons();
+		$("#actor-input").val("");
 	}
 });
 //on-click event that lets user play and pause gif at will
-$(document.body).on("click", ".gif", function() {
+$(document.body).on("click", ".gif", function () {
 	//Assigns state variable to the value of data-state attribute
 	let state = $(this).attr("data-state");
 	//If statement that determines if gif is in still state
